3cf683f5fdeb5c3ef0b31feb2e1a9d5633982b07
Elena <qa2301-mail.ru>
2025-01-09 23:44:44 +0300

    Added manager and tests

---
+++ src/main/java/ru/netology/service/FilmManager.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+public class FilmManager {
+
+    private Film[] films = new Film[0];
+    private int size;
+
+    public FilmManager() {
+        this.size = 5;
+    }
+
+    public FilmManager(int size) {
+        this.size = size;
+    }
+
+    // Добавление фильма
+    public void addFilm(Film film){
+        Film[] tmp = new Film[films.length + 1];
+
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        this.films = tmp;
+    }
+
+    // Вывод все фильмов в порядке добавления
+
+    public Film[] findAll() {
+        return films;
+    }
+
+    // Вывод фильмов в порядке добавления с возможностью выбора количества фильмов: по умолчанию или в выбранном диапазоне
+    public Film[] findLast() {
+        int resultLength;
+        if (films.length < size) {
+            resultLength = films.length;
+        } else {
+            resultLength = size;
+        }
+
+        Film[] reverse = new Film[resultLength];
+        for (int i = 0; i < reverse.length; i++) {
+            reverse[i] = films[films.length - 1 -i];
+        }
+        return reverse;
+    }
+
+}
